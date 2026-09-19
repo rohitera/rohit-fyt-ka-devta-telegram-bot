037288986bf72f31fa101aa20bb40ee72481a257
rohitera <myusuf.lenge@gmail.com>
2026-09-19 20:02:57 +0530

    Suppress Telegram token URLs in logs

--- bot.py
+++ bot.py
@@ -174,6 +174,8 @@ logging.basicConfig(level=logging.INFO)
 
 
 logger = logging.getLogger(__name__)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpcore").setLevel(logging.WARNING)
 
 
 
